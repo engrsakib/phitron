650cc9ae2c38d251ab6cd45009d802a7c801d86b
engrsakib <snazmussakib01@gmail.com>
2024-03-16 12:23:25 +0600

    day 2 upd

---
+++ XPSC/weekly Task/week 1/Day 2/saki.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Md. Nazmus Sakib
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
