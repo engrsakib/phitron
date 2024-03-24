dfdd64fdcf0cc11b6ad0150dd9ed779fa3247540
engrsakib <snazmussakib01@gmail.com>
2024-03-25 02:22:18 +0600

    update

--- XPSC/weekly Task/week 2/Day 3/M_Kalindrome_Array.cpp
+++ XPSC/weekly Task/week 2/Day 3/M_Kalindrome_Array.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 
 using namespace std;
 
 int main()
 {
+    // _Created : 24 March 2024 ||  01:32:04
+    // _File    : M_Kalindrome_Array.cpp
     // Md. Nazmus Sakib
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // اللهم صل على سيدنا محمدن النبى الامى وآله وسلم
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
@@ -14,73 +19,64 @@ int main()
     {
         int n;
         cin >> n;
-        vector<int> arr(n);
-        int count = 0;
-        for (int i = 0; i < n; i++)
+        vector<int> arr(n + 1);
+        for (int i = 1; i <= n; i++)
         {
             cin >> arr[i];
         }
-        vector<int> pln1;
-        vector<int> pln2;
-        int g, h;
-        bool flag = true;
-        for (int i = 0; i < n; i++)
+        vector<int> v;
+        int l = -1, r = -1;
+        for (int i = 1; i <= n / 2; i++)
         {
-            if (arr[i] != arr[n - i])
+            if (arr[i] != arr[n - i + 1])
             {
-                flag = false;
-                g = arr[i];
-                h = arr[n - 2 - i];
+                l = arr[i];
+                r = arr[n - i + 1];
+                break;
             }
         }
-        if (!flag)
+        bool flag = true;
+        if (l == -1)
         {
-            for (int i = 0; i < n; i++)
+            flag = true;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (arr[i] == l)
             {
-                if (arr[i] != g)
-                {
-                    pln1.push_back(arr[i]);
-                }
-                if (arr[i] != h)
-                {
-                    pln2.push_back(arr[i]);
-                }
+                continue;
             }
-            bool flag1 = true;
-            int l = pln1.size() - 1;
-            bool flag2 = true;
-            int r = pln2.size() - 1;
-
-            for (int i = 0; i < l; i++)
-            {
-                if (pln1[i] != pln1[l])
-                {
-                    flag1 = false;
-                }
-            }
-
-            for (int i = 0; i < r; i++)
-            {
-                if (pln2[i] != pln2[r])
-                {
-                    flag2 = false;
-                }
-            }
-
-            if (flag1 || flag2)
-            {
-                cout << "YES" << endl;
-            }
-            else
-            {
-                cout << "NO" << endl;
-            }
+            v.emplace_back(arr[i]);
+        }
+        vector<int> b = v;
+        reverse(v.begin(), v.end());
+        if (b == v)
+        {
+            flag = true;
         }
         else
         {
-            cout << "YES" << endl;
+            v.clear();
+            b.clear();
+            for (int i = 1; i <= n; i++)
+            {
+                if (arr[i] == r)
+                {
+                    continue;
+                }
+                v.emplace_back(arr[i]);
+            }
+            b = v;
+            reverse(v.begin(), v.end());
+            if (v == b)
+                flag = true;
+            else
+                flag = false;
         }
+        if (flag)
+            yes;
+        else
+            no;
     }
-
     return 0;
 }
