6c11ddf2791cdd15f7608a68c419fe95a04dd768
engrsakib <snazmussakib01@gmail.com>
2024-04-07 19:36:54 +0600

    day 4

---
+++ XPSC/weekly Task/week 4/day 3/A_Min_Or_Sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define br "\n"
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+using namespace std;
+
+int main()
+{
+    // _Created :  07 April 2024 ||   19:25:10
+    // _File    :  A_Min_Or_Sum.cpp
+    // Writer    :  Md. Nazmus Sakib
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // اللهم صل على سيدنا محمدن النبى الامى وآله وسلم
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        ll ans = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            ans |= v[i];
+        }
+        cout << ans << br;
+    }
+    return 0;
+}
