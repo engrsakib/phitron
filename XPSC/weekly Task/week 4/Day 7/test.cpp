3335a8f6f60914ba28d1c06e94f9775db3732267
engrsakib <snazmussakib01@gmail.com>
2024-04-17 23:19:03 +0600

    test

---
+++ XPSC/weekly Task/week 4/Day 7/test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define br "\n"
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+using namespace std;
+
+int main()
+{
+    // _Created :  17 April 2024 ||   23:16:33
+    // _File    :  test.cpp
+    // Writer    :  Md. Nazmus Sakib
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // اللهم صل على سيدنا محمدن النبى الامى وآله وسلم
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "YES" << br;
+    return 0;
+}
