937755bca0cf023416092431fd65a68f5e8c5c60
engrsakib <snazmussakib01@gmail.com>
2024-04-19 21:07:34 +0600

    day 1

--- XPSC/weekly Task/week 5/day 1/B_Array_Cancellation.cpp
+++ XPSC/weekly Task/week 5/day 1/B_Array_Cancellation.cpp
@@ -1,30 +1,44 @@
 #include <bits/stdc++.h>
+
+#define br "\n"
 #define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 using namespace std;
 
 int main()
 {
-
+    // _Created :  19 April 2024 ||   20:52:28
+    // _File    :  B_Array_Cancellation.cpp
+    // Writer    :  Md. Nazmus Sakib
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // اللهم صل على سيدنا محمدن النبى الامى وآله وسلم
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     ll t;
     cin >> t;
-
-read:
     while (t--)
     {
         ll n;
         cin >> n;
-
-        ll pos = 0, neg = 0;
-        for (ll i = 1; i <= n; i++)
+        vector<ll> v(n);
+        for (ll i = 0; i < n; i++)
         {
-            ll x;
-            cin >> x;
+            cin >> v[i];
+        }
+        ll pos = 0, neg = 0;
+        for (ll x : v)
+        {
             if (x >= 0)
+            {
                 pos += x;
+            }
             else
             {
                 if (pos + x >= 0)
+                {
                     pos += x;
+                }
                 else
                 {
                     x += pos;
@@ -33,9 +47,7 @@ read:
                 }
             }
         }
-
-        cout << -neg << endl;
+        cout << pos << br;
     }
-
     return 0;
 }
