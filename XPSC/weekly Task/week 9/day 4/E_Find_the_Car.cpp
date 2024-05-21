ec99be9d010800e2cd7b7c92488abd9c3701e69c
engrsakib <snazmussakib01@gmail.com>
2024-05-21 22:03:39 +0600

    day 7

--- XPSC/weekly Task/week 9/day 4/E_Find_the_Car.cpp
+++ XPSC/weekly Task/week 9/day 4/E_Find_the_Car.cpp
@@ -13,14 +13,14 @@ public:
     {
         int n, k, q;
         cin >> n >> k >> q;
-        vector<ll> a(k + 1);
+        vector<ll> v(k + 1);
         vector<ll> b(k + 1);
 
-        a[0] = 0;
+        v[0] = 0;
         b[0] = 0;
         for (int i = 1; i <= k; i++)
         {
-            cin >> a[i];
+            cin >> v[i];
         }
         for (int i = 1; i <= k; i++)
         {
@@ -37,7 +37,7 @@ public:
             while (l <= r)
             {
                 ll mid = l + r >> 1;
-                if (a[mid] > c)
+                if (v[mid] > c)
                 {
                     r = mid - 1;
                 }
@@ -46,17 +46,17 @@ public:
                     l = mid + 1;
                 }
             }
-            if (a[r] == c)
+            if (v[r] == c)
             {
                 cout << b[r] << " ";
                 continue;
             }
             
-            ll ans = b[r] + (c - a[r]) * (b[r + 1] - b[r]) / (a[r + 1] - a[r]);
+            ll ans = b[r] + (c - v[r]) * (b[r + 1] - b[r]) / (v[r + 1] - v[r]);
             
             cout << ans << " ";
         }
-        cout << endl;
+        cout << br;
     }
 };
 signed main()
